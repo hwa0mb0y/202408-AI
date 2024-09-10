cb60a3ed1d9befb272107d76fcbb106e13a1488b
hwaomboy <hwaomboy@gmail.com>
2024-09-10 13:30:49 +0900

    01

--- class01.py
+++ class01.py
@@ -6,6 +6,7 @@
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
+from huggingface_hub import HuggingFaceHub
 
 from getpass import getpass
 import os
@@ -19,7 +20,11 @@ os.environ["OPENAI_API_KEY"] = api_key
 # temperature=0.7은 모델의 창의성 수준을 설정
 # 2024년 8월 기준 GPT-3.5-turbo 사용
 # llm = OpenAI(temperature=0.7, model="gpt-3.5-turbo")
-llm = OpenAI(temperature=0.7, max_tokens=700)  
+# llm = OpenAI(temperature=0.7, max_tokens=700)  
+HuggingFaceHub(
+    repo_id="gpt2",  # 모델 이름 (여기서는 "gpt2" 사용)
+    model_kwargs={"temperature": 0.7, "max_length": 100},  # 모델 설정
+)
 
 # 프롬프트 템플릿을 정의합니다.
 # input_variables는 사용자가 입력할 변수를 지정하고, template은 실제 프롬프트의 형식을 정의
@@ -27,7 +32,7 @@ llm = OpenAI(temperature=0.7, max_tokens=700)
 # 정확하게 이야기하면 약간은 다른 성격이지만, template는 시스템 메시지의 작은 한 형태로 이해해도 된다.
 prompt = PromptTemplate(
     input_variables=["topic"],
-    template="{topic}에 대한 흥미로운 사실 세 가지를 알려주세요.",
+    template="{topic}에 대한 한국 내 유명한 블로그 포스트 세 곳을 추천해주세요.",
 )
 
 # LLMChain을 생성합니다.
